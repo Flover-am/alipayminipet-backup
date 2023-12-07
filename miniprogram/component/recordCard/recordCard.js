20c40ed3675ada11d818b2c34137a3e243fd8c83
flover.am-powershall <211250152@smail.nju.edu.cn>
2023-12-07 15:42:24 +0800

    feat: 初步完成记录页

---
+++ miniprogram/component/recordCard/recordCard.js
@@ -0,0 +1,29 @@
+Component({
+  mixins: [],
+  data: {},
+  props: {
+    topRecord: {
+      item1:{
+        name:"name1",
+        value:"20斤"
+      },
+      item2:{
+        name:"name1",
+        value:"20斤"
+      },
+      item3:{
+        name:"name1",
+        value:"20斤"
+      },
+      item4:{
+        name:"name1",
+        value:"20斤"
+      },
+    },
+    title:"TITLE"
+  },
+  didMount() {},
+  didUpdate() {},
+  didUnmount() {},
+  methods: {},
+});
--- miniprogram/pages/records/records.js
+++ miniprogram/pages/records/records.js
@@ -1,7 +1,68 @@
 Page({
   data: {
     petsData: [],
-    userId: 0
+    userId: 0,
+    topRecords: [
+      {
+        item1:{
+          name:"体重",
+          value:"20斤"
+        },
+        item2:{
+          name:"体温",
+          value:"38℃"
+        },
+        item3:{
+          name:"饮食",
+          value:"200g"
+        },
+        item4:{
+          name:"消费",
+          value:"152RMB"
+        },
+      },
+      {
+        item1:{
+          name:"name1",
+          value:"20斤"
+        },
+        item2:{
+          name:"name1",
+          value:"20斤"
+        },
+        item3:{
+          name:"name1",
+          value:"20斤"
+        },
+        item4:{
+          name:"name1",
+          value:"20斤"
+        },
+      },
+      {
+        item1:{
+          name:"name1",
+          value:"20斤"
+        },
+        item2:{
+          name:"name1",
+          value:"20斤"
+        },
+        item3:{
+          name:"name1",
+          value:"20斤"
+        },
+        item4:{
+          name:"name1",
+          value:"20斤"
+        },
+      }
+    ],
+    cardTitles:[
+      "日常记录",
+      "健康记录",
+      "洗护记录"
+    ]
   },
   async onLoad() {
     var self = this;
