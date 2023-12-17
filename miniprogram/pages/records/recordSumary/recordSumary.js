aa3cd017231d46650b94c6249e0c80c89774254e
flover.am-powershall <211250152@smail.nju.edu.cn>
2023-12-17 21:50:41 +0800

    feat: 完成历史时间轴

--- miniprogram/pages/records/recordSumary/recordSumary.js
+++ miniprogram/pages/records/recordSumary/recordSumary.js
@@ -1,36 +1,56 @@
 Page({
   data: {
-    records: [
-      {
-        time: '步骤一',
-      },
-      {
-        time: '步骤二',
-      },
-      {
-        time: '步骤三',
-      },
-      {
-        time: '步骤四',
-      },
-      {
-        time: '步骤五',
-      }
-    ]
+    userId: 0,
+    petId: 0,
+    recordsItem: 0,
+    recordItem:0,
+    history:[{},{}],
+    unit:"kg",
+    petName:"",
+    key:"体温",
+    vary:[]
   },
-  onLoad() {},
-  onCanvasReady() {
-    my.createSelectorQuery().select('#line_pic').node().exec((res) => {
-        const canvas = res[0].node
-        const height_canvas = canvas.height;
-        const width_canvas = canvas.width;
+  caculateVary(){
+    var self = this;
+    if(this.data.history && this.data.history.length >0){
+      var vary = this.data.history.map((value,index,array)=>{
+        if(index===array.length-1){
+          return 0;
+        }else{
+          return value.value - array[index + 1].value;
+        }
+      });
+      self.setData({
+        vary:vary
+      })
+    }
+  },
+  async onLoad() {
+    var self = this;
+    var context = await my.getCloudContext();
+    context.callFunction({
+      name: 'getHistoryFace',
+      data:{
+        userId: self.data.userId,
+        petId: self.data.petId,
+        recordItem: self.data.recordItem,
+        recordsItem:self.data.recordsItem
+      },
+      success:function(res){
+        console.log(res);
+        self.setData({
+          history:res.result.history,
+          unit:res.result.unit,
+          petName:res.result.petName,
+          key:res.result.key
+        });
+        self.caculateVary();
 
-        const ctx = canvas.getContext('2d')
+      }
+    
+      
 
-        ctx.moveTo(width_canvas/2,50);
-        ctx.lineTo(width_canvas/2,height_canvas-50);
-        ctx.stroke();
     })
-
-},
+  },
+  
 });
